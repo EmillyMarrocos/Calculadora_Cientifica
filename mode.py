16e24468ea99ae46715616177dc779e1480c7531
Melissa <m3lgaldin0@gmail.com>
2024-03-25 17:43:19 -0300

    primeiro commit da função mode

---
+++ mode.py
@@ -0,0 +1,7 @@
+def graus_para_radianos(graus):
+    rad = graus * (3.14/180)
+    print(graus, "° graus são ", rad, "radianos.")
+
+def radianos_para_graus(radiano):
+    graus = radiano * (180/3.14)
+    print(radiano, " radianos são ", graus, "° graus.")
