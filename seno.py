37aef7e3978ba92f5d38ac45bae7095b72009b3e
ALUNO SESI PAULISTA <aluno.sesipaulista@SISTEMAFIEPE.LOCAL>
2024-03-25 17:42:59 -0300

    primeiro commit da função seno

---
+++ seno.py
@@ -0,0 +1,8 @@
+cp01 = float(input("Digite o valor do cateto oposto: "))
+h01 = float(input("Digite a hipotenusa: "))
+
+def seno(cp, h):
+    r = cp / h
+    print ("O seno é: ", r)
+
+seno(cp01, h01)
