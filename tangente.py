30c367ac1ecca9b113292ce307ffa500f8026055
Sofiafs <sofiasantosf011@gmail.com>
2024-03-26 16:47:45 -0300

    primeiro commit da função tangente

---
+++ tangente.py
@@ -0,0 +1,7 @@
+def tangente():
+    co = float(input("Digite o valor do cateto oposto: "))
+    ca = float(input("Digite o valor do cateto adjacente: "))
+    r = co / ca
+    print ("O resultado é: ", r)
+
+tangente()
