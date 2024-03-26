f672356d968719e46b8cfd436b814a1f00925d91
Melissa <m3lgaldin0@gmail.com>
2024-03-26 16:41:05 -0300

    primeiro commit da funcao cosseno

---
+++ cosseno.py
@@ -0,0 +1,5 @@
+def cosseno ():
+    hip = float(input('Digite o valor da hipotenusa: '))
+    cat_ad = float(input('Digite o valor do cateto adjacente: '))
+    coss = (cat_ad / hip)
+    print('O valor do cateto oposto é: ', coss)
--- mode.py
+++ mode.py
@@ -1,7 +1,9 @@
-def graus_para_radianos(graus):
+def graus_para_radianos():
+    graus = float(input('Digite o valor em graus: '))
     rad = graus * (3.14/180)
     print(graus, "° graus são ", rad, "radianos.")
 
-def radianos_para_graus(radiano):
+def radianos_para_graus():
+    radiano = float(input('Digite o valor em radiano: '))
     graus = radiano * (180/3.14)
     print(radiano, " radianos são ", graus, "° graus.")
